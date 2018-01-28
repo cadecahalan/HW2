8ccaac4fcfed00df812737d0e278ce1459ab59cc
cadecahalan <cadecahalan@berkeley.edu>
2018-01-28 14:56:50 -0800

    Update 2challenge.js

--- 2challenge.js
+++ 2challenge.js
@@ -1,1 +1,17 @@
+//Mathy Challenge 
 
+function setup() {
+  createCanvas(340, 340);
+}
+
+var slopeLine1 = 40
+var slopeLine2 = 60
+var slopeLine3 = 20
+
+while (true){
+  line(slopeLine1, slopeLine1, slopeLine2, slopeLine3);
+  slopeLine1 = slopeLine1 + 1;
+  slopeLine2 = slopeLine2 + 1;
+  slopeLine3 = slopeLine3 + 1;
+}
+  
