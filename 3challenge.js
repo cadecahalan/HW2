7c2f973538ba71992d3723fc93499ccba055f042
cadecahalan <cadecahalan@berkeley.edu>
2018-01-28 14:16:28 -0800

    Update 3challenge.js

--- 3challenge.js
+++ 3challenge.js
@@ -1,1 +1,14 @@
+background(255);
+colorMode(HSB);
+noStroke();
 
+while(true) {
+  background(0, 0, 255, 0.1);
+  fill(random(200,230), 100, 80);
+  beginShape();
+		vertex(random(30,80), random(20,70));
+		vertex(random(85,135), random(30,80));
+		vertex(random(85,135), random(65,115));
+		vertex(random(30,80), random(30,80));
+		endShape(CLOSE);
+}
