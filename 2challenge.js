e6ff5fa56897ba0c7131c076abc82697e745b2a9
cadecahalan <cadecahalan@berkeley.edu>
2018-02-01 10:24:39 -0800

    Update 2challenge.js

--- 2challenge.js
+++ 2challenge.js
@@ -6,7 +6,7 @@ function setup() {
 
 while (true){
  var lineHoriz = random(height)
- var lineHoriz2 = lineHoriz - lineHoriz
+ var lineHoriz2 = lineHoriz - 2*lineHoriz
  var lineWidth = random(width)
  var lineWidth2 = lineWidth + lineHoriz
  line (lineWidth, lineHoriz, lineWidth2, lineHoriz2);
